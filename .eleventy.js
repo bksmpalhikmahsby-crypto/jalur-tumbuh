fb59a95de5351e286e908db19112e2730894ec05
bksmpalhikmahsby-crypto <bk.smp.alhikmahsby@gmail.com>
2026-06-20 11:34:40 +0700

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("admin");
+
+  eleventyConfig.addCollection("materi", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("content/materi/*.md").sort((a, b) => a.data.num.localeCompare(b.data.num));
+  });
+
+  eleventyConfig.addCollection("profil", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("content/profil/*.md").sort((a, b) => a.data.urutan - b.data.urutan);
+  });
+
+  eleventyConfig.addCollection("berita", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("content/berita/*.md").sort((a, b) => new Date(b.data.tanggal) - new Date(a.data.tanggal));
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
