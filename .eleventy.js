1e1fb88789b69d09b6f638482ebb09c751c986e5
bksmpalhikmahsby-crypto <bk.smp.alhikmahsby@gmail.com>
2026-06-23 16:00:06 +0700

    Add files via upload

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.ignores.add("content/site.md");
 
   eleventyConfig.addCollection("materi", function (collectionApi) {
     return collectionApi.getFilteredByGlob("content/materi/*.md").sort((a, b) => a.data.num.localeCompare(b.data.num));
@@ -16,6 +17,13 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addCollection("video", function (collectionApi) {
     return collectionApi.getFilteredByGlob("content/video/*.md");
+  });
+
+  eleventyConfig.addGlobalData("site", function () {
+    const fs = require("fs");
+    const matter = require("gray-matter");
+    const file = fs.readFileSync("content/site.md", "utf8");
+    return matter(file).data;
   });
 
   return {
